a563332301bb496d44585f4e12b2e2950845d8f4
Gavin McDonald <gavinmcdoh@gmail.com>
2017-02-19 23:31:35 -0500

    cleanup

--- src/js/puppy.jsx
+++ src/js/puppy.jsx
@@ -4,39 +4,12 @@ export default class Puppy extends React.Component {
 	constructor (props) {
 		super(props);
 
-		['getImageURL', 'loadImage', 'loadThumbnail']
+		['getImageURL']
 		.map(method => this[method] = this[method].bind(this));
-
-		this.state = {};
-		this.state.image = new Image();
-		this.state.imageLoaded = false;
-		this.state.thumbnail = new Image();
-		this.state.thumbnailLoaded = false;
 	}
 
-	getImageURL () {
-		return this.props.data.post_hint === 'image' ? this.props.data.url :
-			this.props.data.preview.images[0].source.url;
-	}
-
-	loadImage () {
-		this.state.image.addEventListener('load', event => {
-			this.state.imageLoaded = true;
-			this.forceUpdate();
-		});
-
-		let imageURL = this.getImageURL();
-
-		this.state.image.src = imageURL;
-	}
-
-	loadThumbnail () {
-		this.state.thumbnail.addEventListener('load', () => {
-			this.state.thumbnailLoaded = true;
-			this.forceUpdate();
-		});
-
-		this.state.thumbnail.src = this.props.data.thumbnail;
+	getImageURL (item) {
+		return item.preview.images[0].source.url.replace(/&amp;/g, '&');
 	}
 
 	render () {
@@ -45,7 +18,7 @@ export default class Puppy extends React.Component {
 			zIndex: this.props.primary ? 1 : 0,
 			opacity: this.props.primary && this.props.transitioning ? 0 : 1,
 			backgroundSize: this.props.backgroundSize,
-			backgroundImage: `url(${ this.props.data.preview.images[0].source.url.replace(/&amp;/g, '&') })`
+			backgroundImage: `url(${ this.getImageURL(this.props.data) })`
 		};
 
 		return (
--- src/js/runt.jsx
+++ src/js/runt.jsx
@@ -4,46 +4,19 @@ export default class Runt extends React.Component {
 	constructor (props) {
 		super(props);
 
-		['getImageURL', 'loadImage', 'loadThumbnail']
+		['getImageURL']
 		.map(method => this[method] = this[method].bind(this));
-
-		this.state = {};
-		this.state.image = new Image();
-		this.state.imageLoaded = false;
-		this.state.thumbnail = new Image();
-		this.state.thumbnailLoaded = false;
 	}
 
-	getImageURL () {
-		return this.props.data.post_hint === 'image' ? this.props.data.url :
-			this.props.data.preview.images[0].source.url;
-	}
-
-	loadImage () {
-		this.state.image.addEventListener('load', event => {
-			this.state.imageLoaded = true;
-			this.forceUpdate();
-		});
-
-		let imageURL = this.getImageURL();
-
-		this.state.image.src = imageURL;
-	}
-
-	loadThumbnail () {
-		this.state.thumbnail.addEventListener('load', () => {
-			this.state.thumbnailLoaded = true;
-			this.forceUpdate();
-		});
-
-		this.state.thumbnail.src = this.props.data.thumbnail;
+	getImageURL (item) {
+		return item.preview.images[0].resolutions[0].url.replace(/&amp;/g, '&');
 	}
 
 	render () {
 
 		return (
 			<div className="runt" onClick={this.props.clickHandler} >
-				<img src={`${ this.props.data.preview.images[0].resolutions[0].url.replace(/&amp;/g, '&') || this.props.data.thumbnail  }`} />
+				<img src={`${ this.getImageURL(this.props.data) }`} />
 				<div className="runt-overlay" />
 			</div>
 		);
